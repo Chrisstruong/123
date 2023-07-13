c3a10fd0a8c63fad80a5dcebeeda65a7ac27baa7
chrisstruong <chrisstruong@gmail.com>
2023-07-13 16:37:59 -0400

    Finish set up for backend basically

--- JavaScript-Playground/backend/index.js
+++ JavaScript-Playground/backend/index.js
@@ -1,1 +1,11 @@
-console.log("hello world!")
+const express = require("express")
+
+const app = express() 
+
+app.get('/', (req, res) => {
+    return res.json({hello: "world!"})
+})
+
+app.listen(1000, () => {
+    console.log("listening on port 1000!")
+})
