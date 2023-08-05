8c82ddd8254ec6f8815b4f0c284742f3aeebe398
chrisstruong <chrisstruong@gmail.com>
2023-08-05 11:42:14 -0400

    Update test cases for most frequent char

---
+++ JavaScript-Playground/backend/codes/3a7021ea-9fe6-4682-b4f2-1e4ebcbdc2ed.js
@@ -0,0 +1,35 @@
+// Complexity
+// n = string length
+// Time: O(n)
+// Space: O(n)
+
+const mostFrequentChar = (s) => {
+  // todo
+  
+  // Using Hash Map (Object)
+  const count = {}
+  let max = 0
+  let maxRepeatedChar 
+  
+  for (let char of s) {
+    if (!(char in count)) {
+      count[char] = 0
+    }
+    count[char] += 1
+  }
+  
+  let best = null
+  for (let char of s) {
+    if (best === null || count[char] > count[best]) best = char
+  }
+  return best
+
+
+};
+
+mostFrequentChar('bookeeper')
+
+module.exports = {
+  mostFrequentChar,
+};
+
